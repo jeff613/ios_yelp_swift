a102fb1aa6357252d6e95ef96b9dea7f2412ac34
jeff613 <jeff.ye.613@gmail.com>
2015-02-15 17:25:31 -0800

    first version of the pley custom ylep client

---
+++ Yelp/Business.swift
@@ -0,0 +1,9 @@
+//
+//  Business.swift
+//  Yelp
+//
+//  Created by Jianfeng Ye on 2/10/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import Foundation
---
+++ Yelp/FilterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterTableViewCell.swift
+//  Yelp
+//
+//  Created by Jianfeng Ye on 2/15/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class FilterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
